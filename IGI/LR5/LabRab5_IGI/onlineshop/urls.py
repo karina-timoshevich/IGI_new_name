2c907a1a96ac947905185ecb54886d8b16aea03d
Karina Timoshevich <karina03tima@gmail.com>
2024-05-01 22:09:24 +0300

    Add product detail page

--- IGI/LR5/LabRab5_IGI/onlineshop/urls.py
+++ IGI/LR5/LabRab5_IGI/onlineshop/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
+from django.urls import re_path
 
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('products/', views.ProductListView.as_view(), name='products'),
+#
+#
+#     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+# ]
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('products/', views.ProductListView.as_view(), name='products'),
-
-
-    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^products/$', views.ProductListView.as_view(), name='products'),
+    re_path(r'^product/(?P<pk>\d+)$', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- IGI/LR5/LabRab5_IGI/onlineshop/views.py
+++ IGI/LR5/LabRab5_IGI/onlineshop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Employee, Product, ProductType, Order, Client, Manufacturer
+
+
 # Create your views here.
 
 def index(request):
@@ -7,22 +9,24 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
     # Генерация "количеств" некоторых главных объектов
-    num_products=Product.objects.all().count()
-    num_manufacturers=Manufacturer.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_products = Product.objects.all().count()
+    num_manufacturers = Manufacturer.objects.count()  # Метод 'all()' применён по умолчанию.
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_products':num_products,'num_manufacturers':num_manufacturers},
+        context={'num_products': num_products, 'num_manufacturers': num_manufacturers},
     )
+
 
 from django.views import generic
 
+
 class ProductListView(generic.ListView):
     model = Product
-
+    #paginate_by = 10  для постраничного отображения (надо обрабатывать в base_generic.html)
 
 
 class ProductDetailView(generic.DetailView):
