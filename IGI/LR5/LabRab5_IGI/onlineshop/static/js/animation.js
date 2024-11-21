b8b01f0f69d95eedbe18270780387bc00746c22d
Karina Timoshevich <karina03tima@gmail.com>
2024-11-22 00:20:18 +0300

    add animation during scrolling

---
+++ IGI/LR5/LabRab5_IGI/onlineshop/static/js/animation.js
@@ -0,0 +1,36 @@
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const isScrollingDown = scrollTop > lastScrollTop;
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+
+  const products = document.querySelectorAll(".product-animation-scroll");
+  const cart = document.getElementById("cart-animation-scroll");
+  const cartRect = cart.getBoundingClientRect();
+
+  products.forEach((product, index) => {
+    const productRect = product.getBoundingClientRect();
+    const threshold = window.innerHeight / 2;
+
+    // Проверяем, если продукт уже в корзине
+    const isInCart = product.dataset.inCart === "true";
+
+    if (isScrollingDown && productRect.top < threshold && !isInCart) {
+      // Корректируем смещение по X и Y
+      const offsetX = index*26; // Смещение вправо
+      const offsetY = 0; // Смещение вниз для каждого продукта в корзине
+
+      // Перемещаем продукт в корзину с учетом смещения
+      product.style.transform = `translate(${
+        cartRect.left - productRect.left + offsetX
+      }px, ${cartRect.top - productRect.top + offsetY}px)`;
+
+      product.dataset.inCart = "true"; // Отмечаем, что продукт в корзине
+    } else if (!isScrollingDown && productRect.top < threshold && isInCart) {
+      // При скролле вверх возвращаем на место
+      product.style.transform = "none";
+      product.dataset.inCart = "false"; // Сбрасываем состояние
+    }
+  });
+});
