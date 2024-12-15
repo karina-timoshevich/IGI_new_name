9c7afed5fc3a2112dbf5df1d31ffe03814d97d65
Karina Timoshevich <karina03tima@gmail.com>
2024-12-15 19:55:29 +0300

    add registration

--- STRWEB/LR4/index.js
+++ STRWEB/LR4/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
 import {validationResult} from 'express-validator';
 import {registerValidation} from './validations/auth.js'
-
-mongoose.connect('mongodb://root:example@localhost:27017', {
+import userModel from "./models/user.js"
+mongoose.connect('mongodb://root:example@localhost:27017/onlinestore', {
     authSource: "admin"
 })
 .then(() => console.log('DB ok'))
@@ -15,14 +16,45 @@ const app = express();
 app.use(express.json());
 
 
-app.post('/auth/register',registerValidation, (req, res)=>{
-const errors = validationResult(req);
+app.post('/auth/register',registerValidation, async (req, res)=>{
+try {
+    const errors = validationResult(req);
 if (!errors.isEmpty()){
     return res.status(400).json(errors.array());
 }
-res.json({
-    success: true,
+
+const password = req.body.password;
+const salt = await bcrypt.genSalt(10);
+const hash = await bcrypt.hash(password, salt)
+
+const doc = new userModel({
+    email: req.body.email,
+    name: req.body.name,
+    birthday: req.body.birthday,
+    phone: req.body.phone,
+    passwordHash: hash,
 })
+
+const user = await doc.save()
+const token = jwt.sign({
+    _id: user._id
+}, 'secret123',
+    {
+    expiresIn: '1h',
+    },
+    )
+
+const {passwordHash, ...userData} = user._doc
+res.json({
+    ... userData, token
+})
+}
+catch(err){
+    console.log(err);
+   res.status(500).json({
+       message: "Не удалось зарегистрироваться"
+   })
+}
 })
 
 app.listen(4444, (err)=>{
