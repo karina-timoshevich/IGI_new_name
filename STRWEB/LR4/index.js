dc6380ab7800053e9b44fb3517203c86ae6ea724
Karina Timoshevich <karina03tima@gmail.com>
2024-12-15 17:16:43 +0300

    add validation for register

---
+++ STRWEB/LR4/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+import {validationResult} from 'express-validator';
+import {registerValidation} from './validations/auth.js'
+
+mongoose.connect('mongodb://root:example@localhost:27017', {
+    authSource: "admin"
+})
+.then(() => console.log('DB ok'))
+.catch((err) => console.log('DB error', err));
+
+
+const app = express();
+app.use(express.json());
+
+
+app.post('/auth/register',registerValidation, (req, res)=>{
+const errors = validationResult(req);
+if (!errors.isEmpty()){
+    return res.status(400).json(errors.array());
+}
+res.json({
+    success: true,
+})
+})
+
+app.listen(4444, (err)=>{
+if(err){
+    return console.log(err);
+}
+console.log("Server OK");
+});
---
+++ STRWEB/LR4/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        birthday:{
+            type: Date,
+            required: true,
+        },
+        passwordHash:{
+            type: String,
+            required: true,
+        },
+        phone: String,
+    },
+    {
+        timestamps: true,
+    })
+
+export default mongoose.model('user', userSchema);
